5d018977fd8af1e2fe7201b647f5e78c669c8824
xinxin.yuan <xinxin.yuan@trip.com>
2022-09-16 15:58:24 +0800

    模块化研究

---
+++ dist/esm_import_async_js.js
@@ -0,0 +1,19 @@
+"use strict";
+(self["webpackChunkmodule"] = self["webpackChunkmodule"] || []).push([["esm_import_async_js"],{
+
+/***/ "./esm/import_async.js":
+/*!*****************************!*\
+  !*** ./esm/import_async.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "name": () => (/* binding */ name)
+/* harmony export */ });
+var name = "async";
+
+/***/ })
+
+}]);
+//# sourceMappingURL=esm_import_async_js.js.map
--- esm/index.js
+++ esm/index.js
@@ -1,2 +1,7 @@
 import log, { name } from "./module";
+setTimeout(() => {
+  import("./import_async").then(({ name: async_name }) => {
+    console.log(async_name);
+  });
+}, 3000);
 console.warn("es6 module", log, name);
